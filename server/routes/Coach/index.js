54196cdd57e89b07c7cf1fa83ebb36691d231d7a
Moeed Bhatti <moeedbhatti419@gmail.com>
2023-06-08 13:15:03 +0500

    coach api files added

---
+++ server/routes/Coach/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+// const { myBookings, createBooking, deleteBooking } = require('../../controllers/Bookings')
+
+router.get('/', )
+
+module.exports = router
